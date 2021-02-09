2bb0272156e0edb7a10f88452351482668bcd3bf
Dan Wong <dawong@twilio.com>
2021-02-09 16:18:37 +1100

    pass status back

--- functions/webhook.js
+++ functions/webhook.js
@@ -34,7 +34,7 @@ exports.handler = function(context, event, callback) {
                   console.log(`Call Sid: ${callSid}`);
 
                   client.calls(callSid).update({
-                    twiml: `<Response><Say>You have been verified</Say><Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${context.ACCOUNT_SID}/Flows/FW15328ea9f28f5c3b3250eb87ecc47f3f?FlowEvent=return&VerifyStatus=${challenge.status}</Redirect></Response>`
+                    twiml: `<Response><Say>You have been verified</Say><Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${context.ACCOUNT_SID}/Flows/FW15328ea9f28f5c3b3250eb87ecc47f3f?FlowEvent=return%26VerifyStatus=${challenge.status}</Redirect></Response>`
                   }).then(call => {
                     console.log(`Call Updated: ${call.sid}`);
                     callback(null, {status: "done", call: call}); 
